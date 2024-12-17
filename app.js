87deb121c5851bf66312678519e928241bc219bc
Aman Hussain <160075444+amanhussain12@users.noreply.github.com>
2024-12-18 02:35:42 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+
+                        // chapter NO1
+// Question no 1 & 2
+// alert("Error!Please enter a valid password");
+
+// Question No3
+// alert("Welcome to JS Land... \n Happy coding");
+
+// Question No 4
+// /alert("Welcome to JS Land...");
+// alert("Happy Coding! \n ");
+
+// Question No 5
+// alert("Hello...I can run JS through my web browser's console");
+
+
+                        // chapter NO2
+// Question No 1,2,3&4
+// alert("Aman Hussain");
+// alert("18 years ago");
+// alert("Certified Mobile Application Development");
+
+// Question No 5
+// alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// Question No 6
+// alert("My email address is amanhus977@gmail.com");
+
+// Question No 7
+// alert("I am trying to learn from tha book “A smarter way to learn JavaScript”.");
+
+// Question No 8
+// document.write("Yah! I can write HTML content through  JavaScript");
+
+// Question No 9
+// alert(" “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” ");
+
+
+                        // chapter no3 
+
+// Question No 1
+// alert("I am 18 years old");
+
+// Question No 2
+// alert("You have visited this site 14 times");
+
+// Question No 3
+// document.write("My Birth year is 2006"); 
+// document.write(" Data type of my ddeclared variable is number");
+
+// Question No 4
+// document.write("<b>Aman Hussain</b> ordered <b>5 T-shirt</b>(s) on Kashif Clothing store” "); 
